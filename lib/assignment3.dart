a896ed068b633ac976737cba4ccbd78b3bdf7316
flutter-harshal <nahireharshal.it@gmail.com>
2024-01-30 15:43:58 +0530

    Add Flutter Assignments

---
+++ lib/assignment3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Assignment3 extends StatelessWidget {
+  const Assignment3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+//import 'package:app/assignment2.dart';
+import 'package:app/assignment3.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Assignment3(),
+    );
+  }
+}
